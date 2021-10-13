8fbed31dcbbb1390a949f2fd23fb3442455310bb
Diego Martinez Giardini <diego.martinez.giardini@gmail.com>
2021-10-13 10:45:26 -0300

    Sgudno intento

--- src/animales_git/entidades/Animal.java
+++ src/animales_git/entidades/Animal.java
@@ -9,6 +9,34 @@ package animales_git.entidades;
  *
  * @author diegomartinezgiardini
  */
-public class Animal {
-    
+public abstract class Animal {
+    protected String nombre;
+    protected Integer edad;
+
+    public Animal() {
+    }
+
+    public Animal(String nombre, Integer edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public abstract String mostrar();
+
 }
