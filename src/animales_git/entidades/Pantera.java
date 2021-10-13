5b2c7e9cb9da3b2d010c2801432c5373a3fffdf6
Nicolas Alvarez <anicolas92@hotmail.com>
2021-10-13 11:09:40 -0300

    Nico - Pantera

---
+++ src/animales_git/entidades/Pantera.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animales_git.entidades;
+
+/**
+ *
+ * @author anico
+ */
+public abstract class Pantera extends Animal {
+    
+    private boolean cola;
+    private String color;
+    private final int patas =4;
+
+    public Pantera(boolean cola, String color, String nombre, Integer edad) {
+        super(nombre, edad);
+        this.cola = cola;
+        this.color = color;
+    }
+
+    public Pantera() {
+    }
+
+    public boolean isCola() {
+        return cola;
+    }
+
+    public void setCola(boolean cola) {
+        this.cola = cola;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Pantera{" + "cola=" + cola + ", color=" + color + ", patas=" + patas + '}';
+    }
+    
+    
+}
