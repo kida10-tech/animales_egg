4f04a97febe90a526fd7081082d6e7279cb976ec
Diego Martinez Giardini <diego.martinez.giardini@gmail.com>
2021-10-13 10:29:21 -0300

    Primer commit de animales

---
+++ src/animales_git/entidades/Animal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animales_git.entidades;
+
+/**
+ *
+ * @author diegomartinezgiardini
+ */
+public class Animal {
+    
+}
